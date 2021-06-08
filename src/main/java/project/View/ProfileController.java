bed6ef6068f058e67eccd0bfd39a51037d55639a
MohammadM <mmowlavi2002@gmail.com>
2021-06-08 11:52:29 +0430

    some changes

--- src/main/java/project/View/ProfileController.java
+++ src/main/java/project/View/ProfileController.java
@@ -7,6 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
@@ -14,11 +15,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import project.controller.non_duel.storage.Storage;
@@ -26,31 +30,37 @@ import project.model.User;
 
 public class ProfileController implements Initializable {
     @FXML
-    Circle circleimg;
+    private Rectangle rectangleImage;
     @FXML
-    Button backbtn;
+    private Button backbtn;
     @FXML
-    Button changePasswordbtn;
+    private Button changePasswordbtn;
     @FXML
-    Button changeImagebtn;
+    private Button changeImagebtn;
     @FXML
-    Button changeNicknamebtn;
+    private Button changeNicknamebtn;
     @FXML
-    Label nicknameLabel;
+    private Label nicknameLabel;
     @FXML
-    Label usernameLabel;
+    private Label usernameLabel;
+    @FXML
+    private PasswordField currentPasswordField;
+    @FXML
+    private PasswordField newPasswordField;
+    @FXML
+    private TextField newNicknameField;
+
     private String imageName;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         User user = Storage.getAllUsers().get(0);
         showImage((user.getImagePath()));
-        nicknameLabel.setText("My Nickname : " + user.getNickname());
-        usernameLabel.setText("My Username : " + user.getName());    
+        nicknameLabel.setText(user.getNickname());
+        usernameLabel.setText(user.getName());
     }
 
     private void showImage(String path) {
-        circleimg.setStroke(Color.DARKRED);
         InputStream stream = null;
         try {
             stream = new FileInputStream(path);
@@ -58,25 +68,12 @@ public class ProfileController implements Initializable {
             e.printStackTrace();
         }
         Image image = new Image(stream);
-        circleimg.setFill(new ImagePattern(image));
-        circleimg.setEffect(new DropShadow(+25000d,0d, +2d, Color.BLACK));
+        rectangleImage.setFill(new ImagePattern(image));
+        rectangleImage.setEffect(new DropShadow(+25000d, 0d, +2d, Color.BLACK));
+        rectangleImage.setOpacity(1);
+        rectangleImage.setArcHeight(25);
+        rectangleImage.setArcWidth(25);
         
-    }
-
-    public void changePassword(ActionEvent actionEvent) {
-        try {
-            new MainView().changeView("/project/fxml/changePasswordPage.fxml");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void changeNickname(ActionEvent actionEvent) {
-        try {
-            new MainView().changeView("/project/fxml/changeNicknamePage.fxml");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
     }
 
@@ -107,6 +104,53 @@ public class ProfileController implements Initializable {
             e.printStackTrace();
         }
         return imagePath;
+    }
+
+    public void changePassword(ActionEvent actionEvent) {
+
+        if (currentPasswordField.getText().equals("") || newPasswordField.getText().equals("")) {
+            // label.setStyle("-fx-text-fill:red;-fx-padding:4 0 8 0;-fx-font-weight:bold");
+            // label.setText("FILL FIELDS");
+            // return;
+        }
+        if (!User.getOnlineUser().getPassword().equals(currentPasswordField.getText())) {
+            // label.setStyle("-fx-text-fill:red;-fx-padding:4 0 8 0;-fx-font-weight:bold");
+            // label.setText("CURRENT PASSWORD IS WRONG");
+            // return;
+        }
+        if (newPasswordField.getText().equals(currentPasswordField.getText())) {
+            // label.setStyle("-fx-text-fill:red;-fx-padding:4 0 8 0;-fx-font-weight:bold");
+            // label.setText("ENTER NEW PASSWORD");
+            // return;
+        }
+
+        User.getOnlineUser().setPassword(newPasswordField.getText());
+
+    }
+
+    public void changeNickname(ActionEvent actionEvent) {
+        if (newNicknameField.getText().equals("")) {
+            // label.setStyle("-fx-text-fill:red;-fx-padding:4 0 8 0;-fx-font-weight:bold");
+            // label.setText("FILL FIELDS");
+            // return;
+        }
+        if (doesUserWithThisNicknameAlreadyExists()) {
+            // label.setStyle("-fx-text-fill:red;-fx-padding:4 0 8 0;-fx-font-weight:bold");
+            // label.setText("NEW NICKNAME IS REPEATED");
+            // return;
+        }
+
+        Storage.getAllUsers().get(0).setNickname(newNicknameField.getText());
+
+    }
+
+    private boolean doesUserWithThisNicknameAlreadyExists() {
+        ArrayList<User> allUsers = Storage.getAllUsers();
+        for (int i = 0; i < allUsers.size(); i++) {
+            if (allUsers.get(i).getNickname().equals(newNicknameField.getText()))
+                return true;
+        }
+        return false;
     }
 
     public void backToMainMenu() {
