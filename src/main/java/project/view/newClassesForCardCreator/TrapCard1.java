34771e48e584c478550fb5e61fde0861df24e93a
MohammadM <mmowlavi2002@gmail.com>
2021-07-09 18:01:59 +0430

    nothing

--- src/main/java/project/view/newClassesForCardCreator/TrapCard1.java
+++ src/main/java/project/view/newClassesForCardCreator/TrapCard1.java
@@ -10,5 +10,5 @@ import java.util.List;
 public class TrapCard1 {
     public TrapCard1(String cardName, String cardDescription, TrapCardValue trapCardValue, CardPosition notApplicable, int numberOfAllowedUsages, int numberOfTurnsForActivationForTrapCard, int i, HashMap<String, List<String>> hashMapEffects, Image cardImage, HashMap<String, Integer> numbersOfEffectsToSend) {
 
-    }
+    } 
 }
