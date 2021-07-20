818f53d8db04a93efaa5e6e15f01ea212ebf0878
mjmaher987 <mjmaher987@gmail.com>
2021-07-20 17:57:02 +0430

    //some changes for auction

--- src/main/java/project/model/Auction.java
+++ src/main/java/project/model/Auction.java
@@ -1,9 +1,15 @@
 package project.model;
 
+import com.google.gson.JsonObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Auction {
     private String auctionCreatorName;
     private int price;
     private String bestBuyerName;
+    private static int auctionCode = 1;
 //    private static HashMap<String, Integer>
     private static String addressOfStorage = "Resourses\\Server\\";
 
@@ -11,6 +17,24 @@ public class Auction {
         this.auctionCreatorName = auctionCreatorName;
         this.price = initialPrice;
         this.bestBuyerName = "";
+
+            try {
+                FileWriter fileWriter = new FileWriter(
+                    addressOfStorage + "Auctions\\" + auctionCode + ".json");
+                fileWriter.write(toGsonFormat(this));
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+    }
+
+    private String toGsonFormat(Auction auction) {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("auctionCreatorName", auction.getAuctionCreatorName());
+        jsonObject.addProperty("bestBuyerName", auction.getBestBuyerName());
+        jsonObject.addProperty("price", auction.getPrice());
+        return jsonObject.toString();
     }
 
     public String getAuctionCreatorName() {
