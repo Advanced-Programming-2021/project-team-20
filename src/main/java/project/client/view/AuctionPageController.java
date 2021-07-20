f6d72e47990ba5c45de928a10a7ce1c9ff573bcb
mjmaher987 <mjmaher987@gmail.com>
2021-07-20 17:20:13 +0430

    /rollBack changes of AuctionPageController and start creating it from first

--- src/main/java/project/client/view/AuctionPageController.java
+++ src/main/java/project/client/view/AuctionPageController.java
@@ -1,56 +1,44 @@
 package project.client.view;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class AuctionPageController implements Initializable {
 
-    @FXML
-    private Button backbtn;
-    @FXML
-    private Button activateDeckbtn;
-    @FXML
-    private Label activatedStatusLabel;
-    @FXML
-    private Label deckNameLabel;
-    private static List<Rectangle> allMainDeckRectangle;
-    private static List<Rectangle> allSideDeckRectangle;
-    private static List<Rectangle> allScrollBarRectangle;
-    private static List<Rectangle> allScrollBarBackGroundRectangles;
-    private static AnchorPane anchorPane;
-    private static String deckname;
-    private Rectangle shownCardRectangle;
-    private static List<Label> allCardDiscriptionLabels;
-    private static List<Label> allScrollBarLabels;
-    private static Label sizeOfMainDeckLabel;
-    private static Label sizeOfAllMonsterCardsLabel;
-    private static Label sizeOfAllSpellCardsLabel;
-    private static Label sizeOfAllTrapCardsLabel;
-    private static boolean isAddedNecessaryThingsForTheFirstTime = false;
-    private static Label equalActivatedStatusLabel;
-    private static Button equalActivateDeckbtn;
-    private static Label equalDeckNameLabel;
+    public Button auctionButton;
+    public Button bidPriceButton;
+    public TableView tableView;
+    public TextField auctionCodeTextField;
+    public TextField priceTextField;
+    public TextField cardNameTextField;
+    public TextField initialPriceTextField;
+
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        SongPlayer.getInstance().pauseMusic();
-        SongPlayer.getInstance().prepareBackgroundMusic("/project/ingameicons/music/DeckMenu.mp3");
-        equalActivatedStatusLabel = activatedStatusLabel;
-        equalActivateDeckbtn = activateDeckbtn;
-        equalDeckNameLabel = deckNameLabel;
+        createTable();
     }
 
+    private void createTable() {
 
+    }
 
     public void auctionFunction(ActionEvent actionEvent) {
+
     }
+
+    public void bidPriceFunction(ActionEvent actionEvent) {
+
+    }
+
+
+//    private static void continueCreatingTable(){
+//
+//    }
 }
