af621b4ee673281dc972bb48fbadb303f783eb00
Pooya2002 <pooyaesmakh.81@yahoo.com>
2021-08-27 11:02:57 +0430

    adding TrapEffectEnums in model

---
+++ src/main/java/project/model/TrapEffectEnums/ContinuousTrapCardEffect.java
@@ -0,0 +1,6 @@
+package project.model.TrapEffectEnums;
+
+public enum ContinuousTrapCardEffect {
+    THIS_CARD_IS_TIED_TO_THE_SPECIAL_SUMMONED_MONSTER,
+    ALL_LEVEL_4_OR_HIGHER_MONSTERS_CANT_ATTACK,
+}
---
+++ src/main/java/project/model/TrapEffectEnums/NormalSummonTrapCardEffect.java
@@ -0,0 +1,8 @@
+package project.model.TrapEffectEnums;
+
+public enum NormalSummonTrapCardEffect {
+    IF_ATK_IS_AT_LEAST_1000_ATK_DESTROY_IT,
+    DESTROY_ALL_MONSTERS_ON_FIELD,
+    PAY_2000_LP,
+    NEGATE_ACTIVATION_OR_SUMMONING_AND_DESTROY_CARD,
+}
