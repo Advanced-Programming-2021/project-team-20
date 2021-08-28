32efdbf8b9e632573328c64d80483277460964c1
Pooya2002 <pooyaesmakh.81@yahoo.com>
2021-08-28 22:01:57 +0430

    adding module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,25 @@
+module PhaseOneProject {
+    requires gson;
+    requires opencsv;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+    requires java.sql;
+    requires java.desktop;
+
+
+    opens project.server to javafx.fxml;
+    exports project.server;
+    opens project.client.view to javafx.fxml;
+    exports project.client.view;
+    opens project.client.view.pooyaviewpackage to javafx.fxml;
+    exports project.client.view.pooyaviewpackage;
+    opens project.client.view.pooyaviewpackage.unused to javafx.fxml;
+    exports project.client.view.pooyaviewpackage.unused;
+    opens project.client.view.internet to javafx.fxml;
+    exports project.client.view.internet;
+    exports project.client.view.newClassesForCardCreator;
+    opens project.client.view.newClassesForCardCreator to javafx.fxml;
+    exports project.client.view.Components;
+    opens project.client.view.Components to javafx.base, javafx.fxml;
+}
